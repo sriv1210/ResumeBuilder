6d7ea616ad6ac840e6c33bdd00f536ee8dfe29a9
sriv1210 <sripradha1210@gmail.com>
2022-01-10 13:40:16 +0530

    commit

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -14,7 +14,7 @@ export default class Navigation extends Component {
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#about">Resume Builder</a></li>
                             <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Resume Templates</a></li>
-                            <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Track Application</a></li>
+                            {/* <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Track Application</a></li> */}
                         </ul>
                     </div>
                 </div>
